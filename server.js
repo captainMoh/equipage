64427c4e6ea6056b265eb8fa84032de60672de41
captainMoh <aichoun026@gmail.com>
2022-10-30 16:14:33 +0100

    cors

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const app = express();
 const path = require('path');
 require('dotenv').config({path: './config/.env'});
 require('./config/db');
+const cors = require('cors');
 const membreRoute = require('./routes/membreRoutes');
 
 
+app.use(cors())
 app.use(express.static('client/build'));
 app.use(express.json());
 
